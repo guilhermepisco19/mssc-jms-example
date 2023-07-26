5aa381674d4fa4a8f00b14819f610d0a1e9c6dee
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2023-07-26 17:32:01 +0100

    First commit

---
+++ src/main/java/dev/guilhermepisco/msscjmsexample/MsscJmsExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.guilhermepisco.msscjmsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscJmsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/guilhermepisco/msscjmsexample/MsscJmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermepisco.msscjmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJmsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
